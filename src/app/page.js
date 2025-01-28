6f312313df0764a0b574b0071702070bb15718aa
shishpal-2000 <shishpal@dkcexport.co.in>
2025-01-28 12:13:14 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your changes instantly frontend.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
